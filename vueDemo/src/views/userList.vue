a4f3e392fee02ef9a3361e072c51ac8e4c11aff1
Willmind <tanzhijingege@126.com>
2020-01-20 21:35:40 +0800

    更新id删除

--- vueDemo/src/views/userList.vue
+++ vueDemo/src/views/userList.vue
@@ -3,7 +3,11 @@
     <el-form>
       <h2>列表信息</h2>
       <el-form-item>
-        <el-table :data="tableData" border style="width: 300px">
+        <el-table :data="tableData" border style="width: 500px">
+          <el-table-column
+            prop="id"
+            label="id">
+          </el-table-column>
           <el-table-column
             prop="name"
             label="用户名">
@@ -33,6 +37,8 @@
 
       <el-form-item>
         <el-button @click="back()">返回</el-button>
+        <el-button @click="refresh()" :loading="loading">{{ loading ? '刷新中 ...' : '刷 新' }}</el-button>
+
 
       </el-form-item>
     </el-form>
@@ -48,21 +54,32 @@
     export default {
         name: "userList",
         data() {
+
             return {
+                loading:false,
                 tableData: [],
             }
         },
         methods: {
+            refresh(){
+                this.loading = true;
+                this.getDataList()
+                setTimeout(() => {
+                    this.loading = false;
+                }, 200);
+                console.log(1);
+
+            },
             getDataList() {
                 axios.post("/users/userList").then((response) => {
                     this.tableData = response.data.result
+                    console.log(this.tableData);
                 })
             },
             back() {
                 this.$emit('goBack');
             },
             handleDelete(index, row) {
-                console.log(row);
                 this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
@@ -81,9 +98,9 @@
                 });
             },
             del(row){
-                console.log(row.name);
+                console.log(row.id);
                 axios.post("/users/deleteUser",{
-                    name:row.name
+                    id:row.id
                 }).then((response)=>{
                     console.log(response);
                     this.getDataList()
