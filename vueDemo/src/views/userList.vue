7bba568bb0d961889a1fe83028828730e7c6a9a5
Willmind <tanzhijingege@126.com>
2020-01-20 20:56:21 +0800

    更新删除

--- vueDemo/src/views/userList.vue
+++ vueDemo/src/views/userList.vue
@@ -14,13 +14,25 @@
             label="密码">
           </el-table-column>
 
+          <el-table-column
+            label="操作">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)"
+              >删除
+              </el-button>
+            </template>
+          </el-table-column>
+
 
         </el-table>
 
       </el-form-item>
 
       <el-form-item>
-        <el-button  @click="back()">返回</el-button>
+        <el-button @click="back()">返回</el-button>
 
       </el-form-item>
     </el-form>
@@ -32,6 +44,7 @@
 
 <script>
     import axios from 'axios'
+
     export default {
         name: "userList",
         data() {
@@ -39,16 +52,45 @@
                 tableData: [],
             }
         },
-        methods:{
-            getDataList(){
-                axios.post("/users/userList").then((response)=>{
-                    this.tableData=response.data.result
-                    console.log(response.data.result);
+        methods: {
+            getDataList() {
+                axios.post("/users/userList").then((response) => {
+                    this.tableData = response.data.result
                 })
             },
-            back(){
+            back() {
                 this.$emit('goBack');
             },
+            handleDelete(index, row) {
+                console.log(row);
+                this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.del(row)
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            del(row){
+                console.log(row.name);
+                axios.post("/users/deleteUser",{
+                    name:row.name
+                }).then((response)=>{
+                    console.log(response);
+                    this.getDataList()
+                })
+
+            }
+
         },
         mounted() {
             this.getDataList()
